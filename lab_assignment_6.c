c5c21295d896063f17588e7f0070498f14f19a96
melanieprado <134087719+melanieprado@users.noreply.github.com>
2023-06-23 18:11:20 -0400

    Update lab_assignment_6.c

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -6,10 +6,10 @@ int search(int numbers[], int low, int high, int value)
 		return -1;
 	}
 	int mid=(low+high)/2;
-	if(value<array[mid]){
+	if(value<numbers[mid]){
 		return search(numbers, low, mid-1, value);
 	}
-	else if(value>array[mid]){
+	else if(value>numbers[mid]){
 		return search(numbers, mid+1, high, value);
 	}
 	else{
